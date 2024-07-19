82eb67d6bbfd5aa212c8f71c968f24a5c03c241e
Juan José Jiménez Monsalve <jjsanchez689@gmail.com>
2024-07-19 18:53:14 -0500

    feat: add home page to example

--- example/lib/src/screens/home_screen.dart
+++ example/lib/src/screens/home_screen.dart
@@ -10,15 +10,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(title: 'Aplicación de ejemplo!'),
+      appBar: const CustomAppBar(title: 'Aplicación de ejemplo'),
       body: Center(
-        child: Column(
+        child: ListView(
           children: [
             CardCustom(
-              title: 'Hola!',
-              subtitle: 'Descripción!!!!!',
+              title: 'Iniciar sesión',
+              subtitle:
+                  'En esta pantalla podrás ver el token al iniciar sesión',
               onTap: () => (),
-            )
+            ),
+            CardCustom(
+              title: 'Obtener carrito',
+              subtitle: 'En esta pantalla podrás ver el carrito de un usuario',
+              onTap: () => (),
+            ),
+            CardCustom(
+              title: 'Agregar / Actualizar producto al carrito',
+              subtitle:
+                  'En esta pantalla podrás agregar o actualizar un producto del carrito',
+              onTap: () => (),
+            ),
+            CardCustom(
+              title: 'Obtener productos de una categoría',
+              subtitle:
+                  'En esta pantalla podrás ver los productos de una categoría',
+              onTap: () => (),
+            ),
+            CardCustom(
+              title: 'Obtener todos los productos',
+              subtitle:
+                  'En esta pantalla podrás ver todos los productos de la tienda',
+              onTap: () => (),
+            ),
+            CardCustom(
+              title: 'Obtener un producto',
+              subtitle: 'En esta pantalla podrás ver un producto en específico',
+              onTap: () => (),
+            ),
+            CardCustom(
+              title: 'Obtener un usuario',
+              subtitle: 'En esta pantalla podrás ver un usuario en específico',
+              onTap: () => (),
+            ),
+            CardCustom(
+              title: 'Agregar un usuario',
+              subtitle:
+                  'En esta pantalla podrás agregar un usuario a la tienda',
+              onTap: () => (),
+            ),
           ],
         ),
       ),
