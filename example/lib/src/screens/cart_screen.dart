4bc27ddea975d2b0c27b042385b666b71de67b7b
Juan José Jiménez Monsalve <jjsanchez689@gmail.com>
2024-07-22 16:21:56 -0500

    feat: add screen cart

--- example/lib/src/screens/cart_screen.dart
+++ example/lib/src/screens/cart_screen.dart
@@ -1,17 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_package_api_fake_store/flutter_package_api_fake_store.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  final CartsHttpService cartsHttpService = CartsHttpService();
+  List<CartModel> _cartItems = [];
+  String? _errorMessage;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCartItems();
+  }
+
+  void _fetchCartItems() async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = null;
+    });
+
+    final result = await cartsHttpService.getCartByUser('1');
+
+    result.fold(
+      (error) {
+        setState(() {
+          _errorMessage = error;
+        });
+      },
+      (cartItems) {
+        setState(() {
+          _cartItems = cartItems;
+        });
+      },
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Obtener carrito'),
+        title: const Text('Carrito'),
       ),
-      body: const Center(
-        child: Text('Pantalla de Obtener carrito'),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : _errorMessage != null
+              ? Center(
+                  child: Text(
+                    _errorMessage!,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                )
+              : _cartItems.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _cartItems.length,
+                      itemBuilder: (context, index) {
+                        final cartItem = _cartItems[index];
+                        return ExpansionTile(
+                          title: Text('Carrito ID: ${cartItem.id}'),
+                          subtitle: Text('Usuario ID: ${cartItem.userId}'),
+                          children: cartItem.products.map((product) {
+                            return ListTile(
+                              title: Text('Producto ID: ${product.quantity}'),
+                              subtitle: Text('Cantidad: ${product.quantity}'),
+                            );
+                          }).toList(),
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text('No hay productos en el carrito.'),
+                    ),
     );
   }
 }
